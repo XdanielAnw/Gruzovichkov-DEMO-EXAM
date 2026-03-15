d908ab41c007f112fa8631103d0a4731fafa3cd9
XdanielAnw <dbaiguzhinov11@mail.ru>
2026-03-15 17:20:56 +0300

    6.0 правильное отображение во view

--- views/account/view.php
+++ views/account/view.php
@@ -6,13 +6,17 @@ use yii\widgets\DetailView;
 /** @var yii\web\View $this */
 /** @var app\models\Application $model */
 
-$this->title = "Заявка №" . $model->id . " от " . Yii::$app->formatter->asDatetime($model->created_at) ;
+$this->title = "Заявка №" . $model->id . " от " . Yii::$app->formatter->asDatetime($model->created_at, "php:Y.m.d H:i:s") ;
 
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="application-view">
 
     <h3><?= Html::encode($this->title) ?></h3>
+
+    <div class="form-group d-flex justify-content-between">
+            <?= Html::a('Назад', ['/account'], ['class' => 'btn btn-outline-info']) ?>
+    </div>
 
     <?= DetailView::widget([
         'model' => $model,
