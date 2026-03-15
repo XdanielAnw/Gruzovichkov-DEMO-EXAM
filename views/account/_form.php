4ec9c4e841817b0d9b7493d54f9af6da5b0df0e3
XdanielAnw <dbaiguzhinov11@mail.ru>
2026-03-15 16:22:37 +0300

    4.2 Сделал правильное отображение выпадающих списков и даты с временем

--- views/account/_form.php
+++ views/account/_form.php
@@ -1,5 +1,8 @@
 <?php
 
+use app\models\AddressFrom;
+use app\models\AddressTo;
+use app\models\BoxType;
 use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
 
@@ -14,19 +17,25 @@ use yii\bootstrap5\ActiveForm;
 
         <h3>Создание заявки</h3>
 
-        <?= $form->field($model, 'box_type_id')->textInput() ?>
+        <?= $form->field($model, 'box_type_id')->dropDownList(
+            BoxType::getBoxType(), ['prompt' => 'Выберите тип груза']
+        ) ?>
 
-        <?= $form->field($model, 'box_date')->textInput() ?>
+        <?= $form->field($model, 'box_date')->textInput(['type' => 'date']) ?>
 
-        <?= $form->field($model, 'box_time')->textInput() ?>
+        <?= $form->field($model, 'box_time')->textInput(['type' => 'time']) ?>
 
         <?= $form->field($model, 'box_ves')->textInput() ?>
 
         <?= $form->field($model, 'box_gabarit')->textInput() ?>
 
-        <?= $form->field($model, 'address_from_id')->textInput() ?>
+        <?= $form->field($model, 'address_from_id')->dropDownList([
+            AddressTo::getAddressTo(), ['prompt' => 'Выберите адрес отправки']
+        ]) ?>
 
-        <?= $form->field($model, 'address_to_id')->textInput() ?>
+        <?= $form->field($model, 'address_to_id')->dropDownList([
+            AddressFrom::getAddressFrom(), ['prompt' => 'Выберите адрес доставки']
+        ]) ?>
 
         <div class="form-group d-flex justify-content-end">
             <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
